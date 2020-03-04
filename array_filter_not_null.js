f92344399117f20cc4b661bebf8b77cf46e5d9c5
xunter <xunterrzn@gmail.com>
2020-03-04 10:52:21 +0300

    array_filter_not_null

---
+++ array_filter_not_null.js
@@ -0,0 +1,15 @@
+
+function array_filter_not_null(arr) {
+    return arr.filter(a => a != null);
+}
+
+if (Array.prototype.notNull != null) {
+    Array.prototype.notNull = function() {
+        return array_filter_not_null(this);
+    }
+}
+if (Array.prototype.filterNotNull != null) {
+    Array.prototype.filterNotNull = function() {
+        return array_filter_not_null(this);
+    }
+}
